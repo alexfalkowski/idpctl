8ebf2b116c35fdd86b9f894ac3200fc1605ce710
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-06 14:23:41 +0100

    feat(init): init idpctl (#2)

--- cmd/client.go
+++ cmd/client.go
@@ -1,13 +1,13 @@
 package cmd
 
 import (
-	"github.com/alexfalkowski/go-client-template/client"
-	"github.com/alexfalkowski/go-client-template/config"
 	"github.com/alexfalkowski/go-service/compress"
 	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/sync"
 	"github.com/alexfalkowski/go-service/telemetry"
+	"github.com/alexfalkowski/idpctl/client"
+	"github.com/alexfalkowski/idpctl/config"
 	"go.uber.org/fx"
 )
 
