25202381842a5b01f1805fdbf3d1be2b7e682b6e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-09 17:18:25 +0100

    test(cli): add start_process (#13)
    This removes some duplication.

--- test/lib/idpctl.rb
+++ test/lib/idpctl.rb
@@ -8,8 +8,13 @@ require 'idpctl/http'
 
 module Idpctl
   class << self
-    def client_config
-      @client_config ||= Nonnative.configurations('.config/client.yml')
+    def start_process(config, flag, param)
+      cmd = Nonnative.go_executable(%w[cover], 'reports', '../idpctl', 'pipeline', '-i', "file:.config/#{config}.yml", flag, param)
+      pid = spawn({}, cmd, %i[out err] => ['reports/pipeline.log', 'a'])
+
+      _, status = Process.waitpid2(pid)
+
+      status
     end
   end
 end
