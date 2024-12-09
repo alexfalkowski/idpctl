415bb36445b167483d70cfc33e960ae9fb1c34c2
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-09 18:42:16 +0100

    fix(cli): verify update format (#15)
    The update format needs to verify that it is id:path.

--- cmd/pipeline/service.go
+++ cmd/pipeline/service.go
@@ -2,6 +2,7 @@ package pipeline
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	"github.com/alexfalkowski/go-service/meta"
@@ -11,6 +12,9 @@ import (
 	"github.com/alexfalkowski/idpctl/pipeline"
 	"github.com/go-resty/resty/v2"
 )
+
+// ErrInvalidUpdateFormat indicates that the format is not how it's expected.
+var ErrInvalidUpdateFormat = errors.New("invalid update format, this should be id:path")
 
 // Service for pipeline.
 type Service struct {
@@ -34,11 +38,7 @@ func (s *Service) Create(ctx context.Context) context.Context {
 	pipeline, err := os.ReadFile(*CreateFlag)
 	runtime.Must(err)
 
-	res, err := s.client.R().
-		SetHeader("Content-Type", "application/json").
-		SetAuthToken(s.token).
-		SetBody(pipeline).
-		Post(s.cfg.Host + "/pipelines")
+	res, err := s.request().SetBody(pipeline).Post(s.cfg.Host + "/pipelines")
 	runtime.Must(err)
 	runtime.Must(rest.Error(res))
 
@@ -49,10 +49,7 @@ func (s *Service) Create(ctx context.Context) context.Context {
 
 // Get a pipelines.
 func (s *Service) Get(ctx context.Context) context.Context {
-	res, err := s.client.R().
-		SetHeader("Content-Type", "application/json").
-		SetAuthToken(s.token).
-		Get(s.cfg.Host + "/pipelines/" + *GetFlag)
+	res, err := s.request().Get(s.cfg.Host + "/pipelines/" + *GetFlag)
 	runtime.Must(err)
 	runtime.Must(rest.Error(res))
 
@@ -64,15 +61,14 @@ func (s *Service) Get(ctx context.Context) context.Context {
 // Update a pipeline.
 func (s *Service) Update(ctx context.Context) context.Context {
 	split := strings.Split(*UpdateFlag, ":")
+	if len(split) != 2 {
+		runtime.Must(ErrInvalidUpdateFormat)
+	}
 
 	pipeline, err := os.ReadFile(split[1])
 	runtime.Must(err)
 
-	res, err := s.client.R().
-		SetHeader("Content-Type", "application/json").
-		SetAuthToken(s.token).
-		SetBody(pipeline).
-		Put(s.cfg.Host + "/pipelines/" + split[0])
+	res, err := s.request().SetAuthToken(s.token).SetBody(pipeline).Put(s.cfg.Host + "/pipelines/" + split[0])
 	runtime.Must(err)
 	runtime.Must(rest.Error(res))
 
@@ -83,10 +79,7 @@ func (s *Service) Update(ctx context.Context) context.Context {
 
 // Delete a pipeline.
 func (s *Service) Delete(ctx context.Context) context.Context {
-	res, err := s.client.R().
-		SetHeader("Content-Type", "application/json").
-		SetAuthToken(s.token).
-		Delete(s.cfg.Host + "/pipelines/" + *DeleteFlag)
+	res, err := s.request().Delete(s.cfg.Host + "/pipelines/" + *DeleteFlag)
 	runtime.Must(err)
 	runtime.Must(rest.Error(res))
 
@@ -97,14 +90,20 @@ func (s *Service) Delete(ctx context.Context) context.Context {
 
 // Delete a pipeline.
 func (s *Service) Trigger(ctx context.Context) context.Context {
-	res, err := s.client.R().
-		SetHeader("Content-Type", "application/json").
-		SetAuthToken(s.token).
-		Post(s.cfg.Host + "/pipelines/" + *TriggerFlag + "/trigger")
+	res, err := s.request().Post(s.cfg.Host + "/pipelines/" + *TriggerFlag + "/trigger")
 	runtime.Must(err)
 	runtime.Must(rest.Error(res))
 
 	ctx = meta.WithAttribute(ctx, "response", meta.String(res.Body()))
 
 	return ctx
+}
+
+func (s *Service) request() *resty.Request {
+	return s.client.R().
+		SetHeaders(map[string]string{
+			"Content-Type": "application/json",
+			"Accept":       "application/json",
+		}).
+		SetAuthToken(s.token)
 }
