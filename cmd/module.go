71994dd69a8093480c9629431d67ab1836efcca1
Alex Falkowski <alexrfalkowski@gmail.com>
2024-12-09 16:25:51 +0100

    fix(cli): check for errors in response
    https://github.com/alexfalkowski/go-service/pull/1061

--- cmd/module.go
+++ cmd/module.go
@@ -3,11 +3,13 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/compress"
+	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/env"
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/sync"
 	"github.com/alexfalkowski/go-service/telemetry"
+	"github.com/alexfalkowski/go-service/transport"
 	"github.com/alexfalkowski/idpctl/config"
 	"go.uber.org/fx"
 )
@@ -15,8 +17,8 @@ import (
 // Module for fx.
 var Module = fx.Options(
 	cmd.Module, env.Module,
-	sync.Module, compress.Module,
-	encoding.Module, feature.Module,
-	telemetry.Module, config.Module,
+	transport.Module, debug.Module,
+	sync.Module, compress.Module, encoding.Module,
+	feature.Module, telemetry.Module, config.Module,
 	fx.Provide(NewVersion),
 )
--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func pipelineCommand(c *sc.Command) {
 	r := c.AddClientCommand("pipeline", "Manage pipelines.", cmd.Module, pipeline.Module)
 	flags.StringVar(r, pipeline.CreateFlag, "create", "c", "", "create a pipeline (path to file)")
 	flags.StringVar(r, pipeline.GetFlag, "get", "g", "", "retrieve a pipeline (the id of the pipeline)")
-	flags.StringVar(r, pipeline.UpdateFlag, "update", "u", "", "update a pipeline (id of the pipeline and the path to file, e.g. id:path")
+	flags.StringVar(r, pipeline.UpdateFlag, "update", "u", "", "update a pipeline (id of the pipeline and the path to file, e.g. id:path)")
 	flags.StringVar(r, pipeline.DeleteFlag, "delete", "d", "", "delete a pipeline (the id of the pipeline)")
 	flags.StringVar(r, pipeline.TriggerFlag, "trigger", "t", "", "trigger a pipeline (the id of the pipeline)")
 }
