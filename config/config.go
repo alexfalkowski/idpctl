2102e554ef902274fc905516033f3084a3b0ba00
Alex Falkowski <alexrfalkowski@gmail.com>
2024-12-10 23:49:44 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.329.0
    https://github.com/alexfalkowski/go-service/pull/1064

--- config/config.go
+++ config/config.go
@@ -1,22 +1,23 @@
 package config
 
 import (
-	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/config"
 	"github.com/alexfalkowski/idpctl/pipeline"
 )
-
-// NewConfig for config.
-func NewConfig(i *cmd.InputConfig) (*Config, error) {
-	c := &Config{}
-
-	return c, i.Decode(c)
-}
 
 // Config for the client.
 type Config struct {
 	Pipeline       *pipeline.Config `yaml:"pipeline,omitempty" json:"pipeline,omitempty" toml:"pipeline,omitempty"`
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
+}
+
+// Valid or error.
+func (c Config) Valid() error {
+	if c.Pipeline == nil || c.Config == nil {
+		return config.ErrInvalidConfig
+	}
+
+	return c.Config.Valid()
 }
 
 func decorateConfig(cfg *Config) *config.Config {
